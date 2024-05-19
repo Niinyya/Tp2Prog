f8a99d64c3fccdb52e7ba49f5b7c4c339f183f48
Samuel Lachapelle <lachapelles23@gmail.com>
2024-05-19 14:32:38 -0400

    Ajout de commentaires pour faciliter la lecture

--- Ex1/Ex1.py
+++ Ex1/Ex1.py
@@ -2,13 +2,13 @@ import json
 import csv
 
 
-def lireFichierJson(nomFichier):
+def lireFichierJson(nomFichier):  # Charge le fichier Json pour permettre la lecture
     with open(nomFichier, "r") as f:
         donnees = json.load(f)
     return donnees
 
 
-def ecrireFichierCsv(nomFichier, fichierJson):
+def ecrireFichierCsv(nomFichier, fichierJson):   # Prend comme paramètre le fichier json et écrit ligne par ligne les nombres
     with open(nomFichier, "w", newline="") as f:
         ecrivainCsv = csv.writer(f)
         ecrivainCsv.writerow(["reel", "imaginaire"])
@@ -16,9 +16,10 @@ def ecrireFichierCsv(nomFichier, fichierJson):
             ecrivainCsv.writerow([nbr[0], nbr[1]])
 
 
-fichierJson = "data.json"
+if __name__ == "__main__":   # main programme
+    fichierJson = "data.json"
 
-fichierCsv = "data.csv"
+    fichierCsv = "data.csv"
 
-lectureFichierJson = lireFichierJson(fichierJson)
-ecrireFichierCsv(fichierCsv, lectureFichierJson)
+    lectureFichierJson = lireFichierJson(fichierJson)
+    ecrireFichierCsv(fichierCsv, lectureFichierJson)
--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -2,14 +2,15 @@ import csv
 
 
 def pokemonCsv(nomFichier):
+    # stockage des pokemons
     pokemonDict = {}
     with open(nomFichier, newline='') as f:
         leacteurCsv = csv.reader(f)
         for ligne in leacteurCsv:
             # print(ligne)
-            nom = ligne[0]
-            stats = list(map(int, ligne[1:]))
-            pokemonDict[nom] = stats
+            nom = ligne[0]  # garde le nom du pokemon
+            stats = list(map(int, ligne[1:]))   # garde les stats du pokemon
+            pokemonDict[nom] = stats    # écrit les stats du pokemon dans un dict avec le nom comme key
     return pokemonDict
 
 
@@ -17,6 +18,9 @@ if __name__ == '__main__':
     pokemon = pokemonCsv("pokemon.csv")
     for nom, stats in pokemon.items():
         print(f"{nom}: {stats}")
+
+
+    # test pour vérification
 
     # print(isinstance(pokemon, dict))
     # print(isinstance(pokemon["Pikachu"], list))
